60b93b599af4d335f925b01305c307bdd3b6b91e
Benish Sarinelli <bsarinelli@gmail.com>
2019-07-01 11:58:35 -0700

    simple logic app complete with Dockerfile index and requirements files

---
+++ index.py
@@ -0,0 +1,8 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def hello():
+	return "Hello World!"
+
+if __name__ == '_main_':
+	app.run(host="0.0.0.0", port=int("5000"), debug=True)
